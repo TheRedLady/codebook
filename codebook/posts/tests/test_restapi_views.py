0bed1afe6994b11b2c406102abb162d342279e6e
TheRedLady <christina.hristakieva@gmail.com>
2017-07-02 10:41:33 +0300

    Add question restapi tests

--- codebook/posts/tests/test_restapi_views.py
+++ codebook/posts/tests/test_restapi_views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from ..models import Question, Vote
+from ..models import Question, Vote, Answer
 from profiles.models import MyUser
 
 
@@ -69,6 +69,82 @@ class QuestionTests(APITestCase):
         user = MyUser.objects.first()
         self.client.force_authenticate(user=user)
         url = reverse('restapi:question-vote', args=(question.id,))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status'], True)
+        self.assertEqual(response.data['vote'], Vote.UP)
+
+
+class AnswerTests(APITestCase):
+
+    def setUp(self):
+        MyUser.objects.create(
+            email='george.michael@gmail.com',
+            password='test',
+            first_name='George',
+            last_name='Michael'
+        )
+        Question.objects.create(
+            title='ABC',
+            content='DEFG',
+            author=MyUser.objects.first()
+        )
+
+    def test_create_answer(self):
+        user = MyUser.objects.first()
+        question = Question.objects.first()
+        data = {
+            'question': question.id,
+            'content': 'This is test content',
+            'author': user.id,
+        }
+
+        self.client.force_authenticate(user=user)
+        url = reverse('restapi:answer-list')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Answer.objects.count(), 1)
+
+    def test_vote_for_answer(self):
+        question = Question.objects.first()
+        user = MyUser.objects.first()
+        answer = Answer.objects.create(
+            question=question,
+            content='answer',
+            author=user
+        )
+        self.client.force_authenticate(user=user)
+        url = reverse('restapi:answer-upvote', args=(answer.id,))
+        response = self.client.post(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Vote.objects.count(), 1)
+        self.assertEqual(Vote.objects.first().vote, Vote.UP)
+
+        url = reverse('restapi:answer-downvote', args=(question.id,))
+        response = self.client.post(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Vote.objects.count(), 1)
+        self.assertEqual(Vote.objects.first().vote, Vote.DOWN)
+        print(answer.votes)
+
+    def test_get_vote_for_answer(self):
+        question = Question.objects.first()
+        user = MyUser.objects.first()
+        answer = Answer.objects.create(
+            question=question,
+            content='answer',
+            author=user
+        )
+        self.client.force_authenticate(user=user)
+        url = reverse('restapi:answer-vote', args=(answer.id,))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['status'], False)
+        self.assertEqual(response.data['vote'], None)
+
+        user.vote_for_answer(answer, Vote.UP)
+
+        url = reverse('restapi:answer-vote', args=(answer.id,))
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['status'], True)
